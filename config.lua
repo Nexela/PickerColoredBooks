dc1d3ea8f20533b87fd16d1994ae6c95deaaec0e
Nicholas Dunn <nexela@gmail.com>
2018-08-19 02:42:44 -0400

    Loads in 0.17

---
+++ config.lua
@@ -0,0 +1,9 @@
+return {
+    red = defines.color.red,
+    orange = defines.color.orange,
+    green = defines.color.green,
+    purple = defines.color.purple,
+    yellow = defines.color.yellow,
+    white = defines.color.white,
+    black = defines.color.black
+}
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend {
+    {
+        type = 'bool-setting',
+        name = 'picker-colored-books',
+        setting_type = 'startup',
+        default_value = true,
+        order = 'picker[startup][colored-blueprints]'
+    }
+}
